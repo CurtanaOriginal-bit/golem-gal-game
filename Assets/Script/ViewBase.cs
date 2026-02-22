06c1e72e869e503f74999fe60e45502d7c0d86c7
kisaragimion <titan7312@gmail.com>
2026-02-22 09:33:41 +0900

    create entry

--- Assets/Script/ViewBase.cs
+++ Assets/Script/ViewBase.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class EntryView : MonoBehaviour
+public class ViewBase : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
